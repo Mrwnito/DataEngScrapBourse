e622b8212628ea3ac8318b0511a2152805363c6a
Mrwnito <150562883+Mrwnito@users.noreply.github.com>
2024-02-09 09:51:25 +0100

    Update mon_spider.py
    ajouts de commentaires + signatures

--- ProjetDataEng/ProjetDataEng/spiders/mon_spider.py
+++ ProjetDataEng/ProjetDataEng/spiders/mon_spider.py
@@ -1,8 +1,16 @@
 import scrapy
 
-#from elasticsearch import Elasticsearch
+# from elasticsearch import Elasticsearch
 
 class MonSpiderSpider(scrapy.Spider):
+    """
+    Spider pour scraper les données des transactions des indices boursiers sur Boursorama.
+
+    Attributs :
+        name (str): Nom unique du spider.
+        indices (dict): Dictionnaire des indices boursiers et leurs codes correspondants sur Boursorama.
+    """
+    
     name = "mon_spider"
     indices = {
         'cac_40': '1rPCAC',
@@ -12,18 +20,37 @@ class MonSpiderSpider(scrapy.Spider):
     }
     
     def start_requests(self):
+        """
+        Méthode pour générer les requêtes initiales pour chaque indice boursier.
+
+        Yields :
+            scrapy.Request: Requête Scrapy pour scraper les données de transactions pour chaque indice.
+        """
         for index_name, index_code in self.indices.items():
             url = f'https://www.boursorama.com/cours/{index_code}/_toutes-les-transactions?limit=2000'
             yield scrapy.Request(url, self.parse, cb_kwargs={'index_name': index_name})
 
     def parse(self, response, index_name):
-        table = response.css('body > div > div > div > div > div.c-block__body > table') # Ignorer la première ligne d'en-tête
-        table = table[0]
+        """
+        Callback pour traiter la réponse de chaque requête générée par start_requests.
+
+        Paramètres :
+            response (scrapy.http.Response): Réponse à la requête.
+            index_name (str): Nom de l'indice boursier correspondant à la réponse.
+
+        Yields :
+            dict: Dictionnaire contenant l'heure, le prix et l'indice de chaque transaction.
+        """
+        # Sélection de la table contenant les données des transactions.
+        table = response.css('body > div > div > div > div > div.c-block__body > table')[0]
         rows = table.css('tbody > tr.c-table__row')
+        
         for row in rows:
+            # Extraction de l'heure et du prix pour chaque transaction.
             time = row.css('td:nth-child(1)::text').get()
             price = row.css('td:nth-child(2)::text').get()
             
+            # Nettoyage et structuration des données extraites.
             if time and price:
                 time = time.strip()
                 price = price.strip()
@@ -34,4 +61,3 @@ class MonSpiderSpider(scrapy.Spider):
                 }
                 
                 yield data
-
