44b47fbfb61896f95c999c36e729f5d950c49672
Mrwnito <150562883+Mrwnito@users.noreply.github.com>
2024-02-09 10:00:18 +0100

    Update pipelines.py
    ajouts de commentaires + signatures

--- ProjetDataEng/ProjetDataEng/pipelines.py
+++ ProjetDataEng/ProjetDataEng/pipelines.py
@@ -1,70 +1,90 @@
 from pymongo import MongoClient
 from itemadapter import ItemAdapter
-#from elasticsearch import Elasticsearch
+# from elasticsearch import Elasticsearch
 import os
 import re
 
 class ProjetdataengPipeline:
+    """
+    Pipeline pour traiter et stocker les éléments scrapés dans MongoDB.
+    
+    Méthodes :
+        open_spider(self, spider) : Initialise la connexion à MongoDB à l'ouverture du spider.
+        close_spider(self, spider) : Ferme la connexion à MongoDB à la fermeture du spider.
+        process_item(self, item, spider) : Traite chaque élément scrapé et l'enregistre dans MongoDB.
+        get_latest_data(index_name) : Récupère les dernières données d'un indice spécifique depuis MongoDB.
+        calculate_percentage_change(clean_data) : Calcule le pourcentage de changement des prix.
+    """
+    
     def open_spider(self, spider):
+        """
+        Initialisation de la connexion à MongoDB et Elasticsearch (si nécessaire) à l'ouverture du spider.
+        """
         mongo_host = os.getenv('MONGO_HOST', 'localhost')
         self.client = MongoClient(f'mongodb://{mongo_host}:27017/')
         self.db = self.client['DATA_db']
 
-        #self.es = Elasticsearch(['http://localhost:9200'])  # Assure-toi que cette URL est correcte
+        # Initialisation d'une connexion à Elasticsearch
+        # self.es = Elasticsearch(['http://localhost:9200'])
 
     def close_spider(self, spider):
+        """
+        Ferme la connexion à MongoDB à la fermeture du spider.
+        """
         self.client.close()
 
     def process_item(self, item, spider):
+        """
+        Traite chaque élément scrapé, le convertit en utilisant ItemAdapter et l'enregistre dans MongoDB.
+        """
         adapter = ItemAdapter(item)
         index_name = adapter['index']
-
-        # Construisez un identifiant unique pour le document, par exemple, en combinant le temps et le prix
         document_id = f"{adapter['time']}-{adapter['price']}"
 
-        # Préparez le document pour l'indexation
-        document_body = {"time": adapter['time'], "price": adapter['price']}
-        # Enregistrement dans MongoDB
+        # Préparation du document pour MongoDB et tentative d'insertion ou de mise à jour
         self.db[adapter['index']].update_one(
             {"time": adapter['time'], "price": adapter['price']},
             {"$setOnInsert": {"time": adapter['time'], "price": adapter['price']}},
             upsert=True
         )
-        
-        # Envoi à Elasticsearch
-        # Indexez le document dans Elasticsearch avec upsert
-        #self.es.update(
-         #   index=index_name, 
-         #   id=document_id, 
-          #  body={"doc": document_body, "doc_as_upsert": True}
-        #)
+
+        # Code pour l'envoi à Elasticsearch (commenté car non utilisé)
+        # self.es.update(
+        #    index=index_name, 
+        #    id=document_id, 
+        #    body={"doc": {"time": adapter['time'], "price": adapter['price']}, "doc_as_upsert": True}
+        # )
         
         return item
-    
+
     @staticmethod
     def get_latest_data(index_name):
+        """
+        Récupère les dernières données pour un indice spécifique depuis MongoDB et nettoie les prix.
+        """
         mongo_host = os.getenv('MONGO_HOST', 'localhost')
         client = MongoClient(f'mongodb://{mongo_host}:27017/')
         db = client['DATA_db']
         collection = db[index_name]
         data = list(collection.find().sort([("time", 1)]).limit(2000))
 
-        # Convertir chaque élément de la liste 'data' séparément
+        # Nettoyage des prix dans les données récupérées
         for entry in data:
             entry['price'] = float(re.sub(r'[^\d.]', '', entry['price']))
 
         client.close()
         return data
-    
+
     @staticmethod
     def calculate_percentage_change(clean_data):
-        if not clean_data:  # Vérifie que clean_data n'est pas vide
+        """
+        Calcule le pourcentage de changement par rapport au premier élément de la liste.
+        """
+        if not clean_data:
             return []
-            
-        reference_value = clean_data[0]['price'] # Le premier prix de la liste est pris comme référence
-        percentage_changes = []
-        for point in clean_data:
-            change = (point['price'] - reference_value) / reference_value * 100
-            percentage_changes.append(round(change, 2))  # Arrondir à deux décimales
+        
+        reference_value = clean_data[0]['price']
+        percentage_changes = [round((point['price'] - reference_value) / reference_value * 100, 2) for point in clean_data]
         return percentage_changes
+
     
